024ba58ec9c8b6e11939efe9d4569d7f1df92434
mayurpatil-13 <mp9112627825@gmail.com>
2023-07-09 00:32:53 +0530

    driver path change

--- Cat_Stubhub.py
+++ Cat_Stubhub.py
@@ -24,62 +24,62 @@ import pymongo
 
 # print("done")
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 
 urls_list = ['https://www.stubhub.com/sports-tickets/category/28/' , 'https://www.stubhub.com/concert-tickets/category/1/' , 'https://www.stubhub.com/theater-and-arts-tickets/category/174/']
 
 categories =[ 'Sports' , 'Concerts' ,'TheatreAndArts']
 i=0
 
-# for url in urls_list:
-url ='https://www.stubhub.com/sports-tickets/category/28/'
-df = pd.DataFrame(columns = ['category_name' ,'EventUrl'])
+for url in urls_list:
+    # url ='https://www.stubhub.com/sports-tickets/category/28/'
+    df = pd.DataFrame(columns = ['category_name' ,'EventUrl'])
 
-driver.get(url)
+    driver.get(url)
 
-wait = WebDriverWait(driver, 20)
-script_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'jsnpzz')))
+    wait = WebDriverWait(driver, 20)
+    script_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'jsnpzz')))
 
 
-arrow = True
+    arrow = True
 
-while(arrow != False):
+    while(arrow != False):
+        WebDriverWait(driver , 20)
+        time.sleep(2)
+        try:
+            element = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "erQXmZ", " " ))]'))
+        )   
+            if element.is_displayed():
+                element.click()
+        except (NoSuchElementException, StaleElementReferenceException, TimeoutException):
+            arrow = False
+
     WebDriverWait(driver , 20)
-    time.sleep(2)
-    try:
-        element = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "erQXmZ", " " ))]'))
-    )   
-        if element.is_displayed():
-            element.click()
-    except (NoSuchElementException, StaleElementReferenceException, TimeoutException):
-        arrow = False
-
-WebDriverWait(driver , 20)
-    
-event_element = driver.find_elements('xpath', '//*[contains(concat( " ", @class, " " ), concat( " ", "gKAnCI", " " ))]/a')
+        
+    event_element = driver.find_elements('xpath', '//*[contains(concat( " ", @class, " " ), concat( " ", "gKAnCI", " " ))]/a')
 
 
-# Extract the content of the script element
-script_content = script_element.get_attribute('innerHTML')
+    # Extract the content of the script element
+    script_content = script_element.get_attribute('innerHTML')
 
-events_list =[]
-print("======")
-print(len(event_element))
-for element in event_element: 
-    event_content = element.get_attribute('href')
-    row_data = {'category_name':categories[i]  ,'EventUrl' : event_content}      
-    df = df.append(row_data,ignore_index = True)
-    # cat_collection.insert_one(row_data)
-    print(event_content)
-    events_list.append(event_content)
-print("======")
-print(len(events_list))
-# Print the script content
-print(script_content)
+    events_list =[]
+    print("======")
+    print(len(event_element))
+    for element in event_element: 
+        event_content = element.get_attribute('href')
+        row_data = {'category_name':categories[i]  ,'EventUrl' : event_content}      
+        df = df.append(row_data,ignore_index = True)
+        # cat_collection.insert_one(row_data)
+        print(event_content)
+        events_list.append(event_content)
+    print("======")
+    print(len(events_list))
+    # Print the script content
+    print(script_content)
 
-df.to_csv(categories[i]+'.csv')
-i+=1
+    df.to_csv(categories[i]+'.csv')
+    i+=1
 
 # Close the WebDriver
 driver.quit()
