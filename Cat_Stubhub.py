b2a225f4472468ec1c9f1723721daaa3b90a942d
mayurpatil-13 <mp9112627825@gmail.com>
2023-07-10 00:02:03 +0530

    driver path change

--- Cat_Stubhub.py
+++ Cat_Stubhub.py
@@ -30,7 +30,7 @@ display.start()
 
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
-chrome_options.addArguments("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(options=chrome_options)
 
