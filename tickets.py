8e123e194bb1db3b75388086e0bf7da2d499aa5c
mayurpatil-13 <mp9112627825@gmail.com>
2023-08-08 15:16:58 +0530

    add tickets.py

---
+++ tickets.py
@@ -0,0 +1,84 @@
+import requests
+from requests.adapters import HTTPAdapter, Retry
+
+from tqdm import tqdm
+import math
+import itertools
+import json
+import time
+import os
+
+
+class EnhancedSession(requests.Session):
+    '''
+    Set timeout at the session level .
+    '''
+    def __init__(self, timeout=(3.05, 4)):
+        self.timeout = timeout
+        return super().__init__()
+
+    def request(self, method, url, **kwargs):
+        # print("EnhancedSession request")
+        if "timeout" not in kwargs:
+            kwargs["timeout"] = self.timeout
+        return super().request(method, url, **kwargs)
+
+def prepare_session():
+    '''
+    Prepare requests session with default timeout and retry.
+    '''
+    s = EnhancedSession(timeout=10)
+    retries = Retry(total=5,
+                    backoff_factor=0.1,
+                    method_whitelist=["POST", "GET"])
+    s.mount('http://', HTTPAdapter(max_retries=retries))
+    s.mount('https://', HTTPAdapter(max_retries=retries))
+    return s
+
+def get_listings(eventid):
+    '''
+    Get all event listings.
+    '''
+    s = prepare_session()
+    
+    url = f"https://www.stubhub.com/event/{eventid}"
+    url = s.request("POST", url,allow_redirects=True).url
+    response = s.request("POST", url)
+
+
+    if response.status_code == 404:
+        print('404, Probably, no listings for event {}'.format(eventid))
+        # print(url)
+        return None
+
+    respjson= response.json()
+    querystring = {"CurrentPage":"1"}
+    total_pages = math.ceil((respjson['TotalCount'] - 2*respjson['PageSize'])/respjson['PageSize'])
+    items = respjson['Items']
+    item_dicts = []
+    with tqdm(total=total_pages, desc=f'Event Id: {eventid}', leave=False) as pbar:
+        while respjson['ItemsRemaining'] > 0:
+            item_dicts.append(items)
+
+            querystring['CurrentPage'] = int(querystring['CurrentPage']) + 1
+            respjson = s.request("POST", url, params=querystring).json()
+            items = respjson['Items']
+            pbar.set_description(f'Code: {response.status_code} Event Id: {eventid}')
+            pbar.update(1)
+            
+        # append last page
+        item_dicts.append(items)
+
+    return list(itertools.chain.from_iterable(item_dicts))
+
+def parse_listing(l):
+    '''
+    Parse listing by extracting relevant columns.
+    '''
+    listing = {}
+    for c in columns:
+        listing[c] = columns[c]
+        
+listings = get_listings(150454889)
+
+print(listings[0])
