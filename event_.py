1b9cad37574c822d95a32e8b9f941f69b17ffb8d
mayurpatil-13 <mp9112627825@gmail.com>
2023-08-08 15:13:28 +0530

    changes in event.py

--- event_.py
+++ event_.py
@@ -1,50 +1,34 @@
-# import time
-# import pandas as pd
 # from selenium import webdriver
-# from selenium.common.exceptions import (NoSuchElementException,
-#                                         StaleElementReferenceException,
-#                                         TimeoutException)
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.support.ui import WebDriverWait
-# import pandas as pd
 # import time
-# import pymongo
-
-# driver = webdriver.Chrome() 
-# driver.maximize_window()
-
-# # client = pymongo.MongoClient('mongodb+srv://aniketchopade2971:Aniket2971@cluster0.scwcgeh.mongodb.net/')
-
-# def getEvent():
- 
-from selenium import webdriver
-import time
 from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common.by import By
+
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) ''Chrome/94.0.4606.81 Safari/537.36')
+# driver = webdriver.Chrome(options=chrome_options)
+# driver.get("view-source:https://www.stubhub.com/philadelphia-eagles-tickets/performer/761/")
+
+# time.sleep(10)
+
+# html_source  = driver.find_element(By.ID,"index-data").get_attribute("innerHTML")
+
+# print(html_source)
+
+# driver.close()
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36')
+
 driver = webdriver.Chrome(options=chrome_options)
-driver.maximize_window()
-driver.get("https://www.stubhub.com/new-york-giants-tickets/performer/6184/")
+driver.get('https://www.stubhub.com/philadelphia-eagles-tickets/performer/761/')
+time.sleep(10)
 
-# Wait for the page to load completely
-time.sleep(20)
+soup = BeautifulSoup(driver.page_source,'html.parser')
+# script  = soup.find("script", {"id": "index-data"}).text.strip()
 
-# Get the page source
-html_source = driver.page_source
-
-# Search for the script element with id "index-data" in the page source
-script_start = html_source.find('<script id="index-data"')
-script_end = html_source.find('</script>', script_start)
-
-if script_start != -1 and script_end != -1:
-    # Extract the script content
-    script_content = html_source[script_start:script_end]
-    print(script_content)
-else:
-    print("Script element not found.")
-
-# Close the WebDriver
-driver.quit()
+print(soup.prettify())
